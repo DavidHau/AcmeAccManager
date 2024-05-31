1d46144c00dad7bcd57e86077a7cc36eb139aa96
David Hau <david.hau@zuhlke.com>
2024-05-31 13:41:25 +0800

    init commit

---
+++ src/main/java/com/acmebank/acmeaccountmanager/AcmeAccountManagerApplication.java
@@ -0,0 +1,13 @@
+package com.acmebank.acmeaccountmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeAccountManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeAccountManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acmebank/acmeaccountmanager/AcmeAccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmebank.acmeaccountmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeAccountManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
